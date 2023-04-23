82ef5024006fdb90e71898ae188d63e67cf6570d
Klaudia <klaudia02460@gmail.com>
2023-04-23 17:08:55 +0200

    Center list and add styling

--- app/src/main/java/pl/put/poznan/michalxpz/compose_snippets/list/MyList.kt
+++ app/src/main/java/pl/put/poznan/michalxpz/compose_snippets/list/MyList.kt
@@ -1,16 +1,16 @@
 package pl.put.poznan.michalxpz.compose_snippets.list
 
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 
 @Composable
 fun MyList(names: List<String>) {
@@ -19,17 +19,33 @@ fun MyList(names: List<String>) {
         horizontalAlignment = Alignment.CenterHorizontally,
         verticalArrangement = Arrangement.Top
     ) {
-        Text(text = "My list")
-        LazyColumn() {
+        Text(
+            text = "Names list",
+            fontSize = 18.sp,
+            fontWeight = FontWeight.SemiBold,
+            modifier = Modifier.padding(8.dp)
+        )
+        LazyColumn(modifier = Modifier
+            .wrapContentSize()
+            .align(Alignment.CenterHorizontally)) {
             itemsIndexed(names) { index, item ->
                 Row(
-                    modifier = Modifier.fillMaxWidth(),
-                    horizontalArrangement = Arrangement.SpaceEvenly
+                    modifier = Modifier.wrapContentSize(),
+                    horizontalArrangement = Arrangement.Start,
+                    verticalAlignment = Alignment.CenterVertically
                 ) {
-                    Text(text = index.toString())
+                    Text(text = index.toString(), modifier = Modifier.padding(end = 24.dp))
+
                     Text(text = item)
                 }
             }
         }
     }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun MyListPreview() {
+    val names = listOf("Basia", "Kasia", "Ola", "Bartek", "Michał", "Klaudia")
+    MyList(names)
 }
