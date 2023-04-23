b45fce2995b25b96b6e2a8658e7b8a2eb0397038
Michał <michauz2001@gmail.com>
2023-04-23 16:34:39 +0200

    state_good

--- app/src/main/java/pl/put/poznan/michalxpz/compose_snippets/counter/Counter.kt
+++ app/src/main/java/pl/put/poznan/michalxpz/compose_snippets/counter/Counter.kt
@@ -6,6 +6,10 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 
@@ -16,7 +20,7 @@ fun Counter() {
         horizontalAlignment = Alignment.CenterHorizontally,
         verticalArrangement = Arrangement.Center
     ) {
-        var timesClicked = 0
+        var timesClicked by remember { mutableStateOf(0) }
         Text(text = "Clicked: $timesClicked")
         Button(onClick = { timesClicked++ }) {
             Text(text = "Click me")
